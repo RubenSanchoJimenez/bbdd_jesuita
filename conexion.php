6d6c31444747b17db235b62653b6b31af3e9128a
ruben <ruben030502@gmail.com>
2023-10-29 10:27:10 +0100

    Cambio de conexión a hosting

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $servidorbd = 'localhost';
-    $usuario = 'root';
-    $contraseña = '';
-    $basedatos = 'jesuitas';
+    $servidorbd = '2daw.esvirgua.com';
+    $usuario = 'user2daw_19';
+    $contraseña = 'z~?R*g9xqKtT';
+    $basedatos = 'user2daw_BD1-19';
